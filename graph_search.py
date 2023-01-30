56c0e9514db3ff1e6c840b83089e69b449f57784
TheJPMZ <monzon.jpmz@gmail.com>
2023-01-30 17:08:33 -0600

    ➕ Search Algorithms

--- graph_search.py
+++ graph_search.py
@@ -1,9 +1,19 @@
 
-def whichever(caminos):
+def breath_first_search(caminos):
     return caminos.pop(0)
     
+    
+def depth_first_search(caminos):
+    return caminos.pop()
+    
+def a_star_search(caminos):
+    road = (min(caminos, key=lambda x: x[-1].heuristic))
+    caminos.remove(road)
+    return road
+    
+    
 
-def graph_search(graph, goal):
+def graph_search(graph, goal, search_algorithm=1):
     caminos = [[graph.initial]]
     explored = []
     
@@ -12,7 +22,7 @@ def graph_search(graph, goal):
         if not caminos: #Si no hay mas caminos
             return False
             
-        current_path = whichever(caminos)
+        current_path = a_star_search(caminos) if search_algorithm == 1 else breath_first_search(caminos) if search_algorithm == 2 else depth_first_search(caminos)
         node = current_path[-1]
         explored.append(node)
         
--- render.py
+++ render.py
@@ -3,23 +3,30 @@ import numpy as np
 import graph_search
 
 def get_cell_type (cell):
+        
+    type = ["⬛", "🏁"] if cell[0] <= 127 else ["🚩", "⬜"]
     
-    CELLS = {
-        (0,0,0) : "⬛",
-        (255,255,255) : "⬜",
-        (255,0,0) : "🚩",
-        (0,255,0) : "🏁",
+    return type[0] if cell[1] <= 127 else type[1] 
+
+def get_color(type) -> tuple:
+    colors: dict = {
+        "⬛": (0,0,0),
+        "⬜": (255,255,255),
+        "🚩": (255,0,0),
+        "🏁": (0,255,0)
     }
+    return colors[type]
     
-    return CELLS.get(tuple(cell), "⬜")
+
 
 class Cell:
     
     def __init__(self, color, x, y):
         self.coords = (x,y)
-        self.color = color
         self.type = get_cell_type(color)
+        self.color = get_color(self.type)
         self.neighbors = []
+        self.heuristic = None
         
     def set_neighbors(self, matrix):
         x,y = self.coords
@@ -36,6 +43,12 @@ class Cell:
         for neighbor in self.neighbors.copy():
             if neighbor.type == "⬛":
                 self.neighbors.remove(neighbor)
+    
+    def distance(self, goal):
+        return (((self.coords[0] - goal.coords[0])**2 + (self.coords[1] - goal.coords[1])**2)**0.5)           
+    
+    def set_heuristic(self, goal):
+        self.heuristic = self.distance(goal)
 
     def __repr__(self) -> str:
         return f"Cell({self.coords}, {self.type})"
@@ -44,12 +57,16 @@ class Graph:
     
     def __init__(self, matrix):
         self.goals = []
+        self.matrix = matrix
         for x in matrix:
             for y in x:
                 if y.type == "🚩":
                     self.initial = y
                 if y.type == "🏁":
                     self.goals.append(y)
+    
+    def __getitem__(self, key):
+        return self.matrix[key]
 
 
 img = Image.open('a.png')
@@ -78,12 +95,55 @@ for x in matrix:
 
 graph = Graph(matrix)
 
+for x in graph:
+    for y in x:
+        y.set_heuristic(graph.goals[0])
+
 ala = graph_search.graph_search(graph, graph.goals[0])
+# TODO: No path found
 for x in ala:
-    x.type = "🔽"
+    x.color = (0,0,255)
 
 
 for x in matrix:
     for y in x:
         print(y.type, end=" ")
     print()
+    
+    
+    
+    
+    
+
+screen_size = 640
+relation = 640/len(matrix)
+    
+#Screen
+
+
+import pygame
+display = pygame.display
+pygame.display.init()
+pygame.display.set_caption("Pathfinding")
+window = pygame.display.set_mode((640,640))
+
+def update_window():
+    display.update()
+
+def draw_grid():
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            pygame.draw.rect(window, graph[x][y].color, (y*relation, x*relation, relation, relation))
+            
+run = True
+while run:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run =  False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            draw_grid()
+    
+    update_window()
+pygame.quit()
